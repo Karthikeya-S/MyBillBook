796e791db71004c6c7426827d533ef54386e076e
Karthikeya S <skarthi2105@gmail.com>
2025-08-30 18:21:44 +0530

    Removing unnecessary files.
    Removing test files

--- src/main/java/com/application/mybillbook/Interfaces/IAuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.mybillbook.Interfaces;
-
-
-import com.application.mybillbook.Entities.Author;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.UUID;
-
-public interface IAuthorRepository extends MongoRepository<Author, UUID> {
-}
--- src/main/java/com/application/mybillbook/MyBillBookApplication.java
+++ src/main/java/com/application/mybillbook/MyBillBookApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class MyBillBookApplication {
 
 	public static void main(String[] args) {
